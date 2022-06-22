67854eab3a63fc295d029b5a5da6d7b24c1d2479
Vincenz Krüsmann <vincenzkruesmann@Vincenzs-MacBook-Pro.local>
2022-06-22 13:57:13 +0200

    add: Kurve class

---
+++ src/main/java/itvm/achtungdiekurve/model/Kurve.java
@@ -0,0 +1,41 @@
+package itvm.achtungdiekurve.model;
+
+public class Kurve {
+
+    private int x;
+    private int y;
+    private String color;
+    private int id;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
