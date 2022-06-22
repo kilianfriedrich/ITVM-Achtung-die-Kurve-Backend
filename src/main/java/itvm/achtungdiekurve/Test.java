6a88cf2a9c15c66b83720d0b177b133d2bace751
Kilian Friedrich <kf769444@fh-muenster.de>
2022-06-22 13:26:17 +0200

    Test commit

--- src/main/java/itvm/achtungdiekurve/Test.java
+++ src/main/java/itvm/achtungdiekurve/Test.java
@@ -1,14 +1,37 @@
 package itvm.achtungdiekurve;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-@RestController
-public class Test {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-    @GetMapping("/test")
-    String test() {
-        return "hallo ben";
+@Service
+public class Test extends TextWebSocketHandler {
+
+    List<WebSocketSession> webSocketSessions = Collections.synchronizedList(new ArrayList<>());
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        super.afterConnectionEstablished(session);
+        webSocketSessions.add(session);
     }
 
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        super.afterConnectionClosed(session, status);
+        webSocketSessions.remove(session);
+    }
+
+    @Override
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+        super.handleMessage(session, message);
+        for (WebSocketSession webSocketSession : webSocketSessions) {
+            webSocketSession.sendMessage(message);
+        }
+    }
 }
